ecab934340c3f9d160e4dcda20d764461a67e367
syahiraaznm <alyssa.aznim@gmail.com>
2020-07-29 15:04:58 +0800

    something

--- resources/views/livewire/board/wallet-market.blade.php
+++ resources/views/livewire/board/wallet-market.blade.php
@@ -5,17 +5,29 @@
         @livewire('component.verticalnav', ['pageName' => 'wallet-market'])
         <div class= "market">
             <h2> Wallet > BTC </h2>
-                <table>
+                <table style= "width: 100%;">
+                
                     <tr>
                         <th>Child Address</th>
-                        <th>User</th>
-                        <th>Balance</th>
-                        <th> </th>
+                        <th>User </th>
+                        <th>Balance </th>
+                        <th> Action </th>
                     </tr>
                     <tr>
-                        <td> </td>
-                        <td> </td>
-                        <td> </td>
+                        <td>... </td>
+                        <td>... </td>
+                        <td>... </td>
+                        <td> <div class="dropdown">
+                            <button class="dropbtn">Action
+                                <div class="dropdown-content">
+                                    <a href="#">Link 1</a>
+                                    <a href="#">Link 2</a>
+                                    <a href="#">Link 3</a>
+                                </div>
+                                
+                            </button>
+                        </div></td>
+                        
                     </tr>
                 </table>
                         
